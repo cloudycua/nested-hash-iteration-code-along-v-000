da32db3fb7636c4c189c049835469a5b57f3e528
Christine Cua <cloudycua@gmail.com>
2018-02-27 23:26:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    binding.pry
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        
 end
